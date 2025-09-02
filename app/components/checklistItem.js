2c12510cd39c889760b0a028d77e1725aa16130f
David Hurrell <dhurrell@hotmail.co.uk>
2025-09-02 16:47:44 +0100

    Add ChecklistItem component with styles and functionality for task management

---
+++ app/components/checklistItem.js
@@ -0,0 +1,32 @@
+"use client";
+import styles from "./checkListItem.module.css";
+
+export default function ChecklistItem({
+  id,
+  task,
+  checked,
+  onToggle,
+  onDelete,
+}) {
+  const handleDelete = () => {
+    alert("Delete function to be implemented");
+  };
+
+  return (
+    <div className={styles.itemContainer}>
+      <li className={styles.checklistItem}>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onToggle}
+          style={{ marginRight: "10px", fontSize: "20px" }}
+          className={styles.checkbox}
+        />
+        {task}
+      </li>
+      <button onClick={() => onDelete(id)} className={styles.deleteButton}>
+        X{" "}
+      </button>
+    </div>
+  );
+}
