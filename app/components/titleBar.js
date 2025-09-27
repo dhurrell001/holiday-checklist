50e8b0fd4b884d021e6e094879aadf851005d799
David Hurrell <dhurrell@hotmail.co.uk>
2025-09-27 15:01:59 +0100

    Refactor TitleBar component: update structure to include heading and commented SVG

--- app/components/titleBar.js
+++ app/components/titleBar.js
@@ -1,4 +1,15 @@
 import styles from "./titleBar.module.css";
 export default function TitleBar() {
-  return <div className={styles.titleContainer}>Holiday Checklist</div>;
+  return (
+    <div className={styles.titleContainer}>
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120">
+        <path
+          fill="#0099ff"
+          fill-opacity="1"
+          d="M0,96L1440,32L1440,0L0,0Z"
+        ></path>
+      </svg> */}
+      <h1>Holiday checkList</h1>
+    </div>
+  );
 }
